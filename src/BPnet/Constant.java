0694f185acf5cf2949e7761acb4516bd7d49cc96
Kqzhang <pigzkqlover@163.com>
2019-07-11 13:27:09 +0800

    删除多余

--- src/BPnet/Constant.java
+++
@@ -1,9 +0,0 @@
-package BPnet;
-
-public class Constant {
-    public static String trainFolder = null;
-    public static int digit = -1;
-    public static int Width = 28;
-    public static int Height = 28;
-}
-
